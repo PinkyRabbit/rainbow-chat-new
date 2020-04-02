9104ec39916fbc2a89e229bd0b2de76502c9cf06
pinkyrabbit <mikita.melnikau@localhost.localdomain>
2020-04-02 17:40:13 +0300

    homepage first component created

--- src-client/src/app/app-routing.module.ts
+++ src-client/src/app/app-routing.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './modules/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+    // canActivate: [AuthGuard]
+    // children: [
+    //   {
+    //     path: 'status',
+    //     loadChildren: './components/home/home.module#HomeModule',
+    //     canActivate: [AuthGuard]
+    //   }
+    // ]
+  }
+  // { path: 'id_token', component: AuthenticationComponent },
+  // { path: 'auth', component: AuthenticationComponent },
+  // { path: 'Unauthorized', component: UnauthorizedComponent },
+  // { path: 'login', component: LoginComponent }
+  // { path: 'logout', component: LogoutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
